83b32d3cacf3e5b8f689f5b7aacf3ee628173a8b
Henry Jr Condolo Gonzalez <henryjuniorcondolo@gmail.com>
2022-10-10 13:28:02 +0200

    Cambios JS

--- Aprendizaje HTML/JS Curso/main.js
+++ Aprendizaje HTML/JS Curso/main.js
@@ -4,14 +4,14 @@ window.onload = () => {
   const form = document.getElementById("todo-form");
   form.onsubmit = (e) => {
     e.preventDefault();
-    const todo = document.getElementById("todo");
-    const todoText = todo.value;
-    todo.value = "";
-    todos.push(todoText);
+    const todoText = document.getElementById("todo");
+    //const todoText = todo.value;
+    
+    todos.push(todoText.value);
+    todoText.value = "";
     const todoList = document.getElementById("todo-list");
-    todoList.innerHTML = "";
-    for (let i = 0; i < todos.length; i++) {
-      todoList.innerHTML += "<li>" + todos[i] + "</li>";
-    }
+    //const todoTemplate = todos.map(t => `<li>${t}</li>`);
+    todoList.innerHTML = todos.map(t => `<li>${t}</li>`).join("");
+    
   };
 };
